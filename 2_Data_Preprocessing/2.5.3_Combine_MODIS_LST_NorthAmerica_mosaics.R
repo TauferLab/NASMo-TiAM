b3d82f79d067469d4b4e981c91b88ff649a6ab48
Ricardo Llamas <37523937+RllamasUdel@users.noreply.github.com>
2023-11-27 18:16:02 -0500

    Update 2.5.3_Combine_MODIS_LST_NorthAmerica_mosaics.R
    minor changes on text comments

--- 2_Data_Preprocessing/2.5.3_Combine_MODIS_LST_NorthAmerica_mosaics.R
+++ 2_Data_Preprocessing/2.5.3_Combine_MODIS_LST_NorthAmerica_mosaics.R
@@ -15,7 +15,7 @@ library(raster)
 
 rasterOptions(tmpdir = "E:/R_tempdirs/", progress = "text", timer = TRUE)
 
-setwd("D:/3_North_America_SM_predictions")
+setwd("E:/3_North_America_SM_predictions")
 
 ##########
 
@@ -100,7 +100,7 @@ for (i in 1:length(years)) {
 ##########
 
 ##Masking of North America combined LST layers with North America Boundary.
-#*This process works in R but takes a long time, so I performed this in ArcPro.
+#*This process works in R but takes a long time, so it was performed in ArcPro.
 
 ##LAEA##
 
@@ -127,7 +127,7 @@ for (i in 1:length(lst_files)) {
 }
 
 ##Reprojection from LAEA to WGS84 and masking with the North American region boundary
-#*This process works in R but takes a long time and does not accurately preserves pixels shape, so I performed this in Arc Pro.
+#*This process works in R but takes a long time and does not accurately preserves pixels shape, so it was performed in ArcPro.
 
 ##WGS84##
 
